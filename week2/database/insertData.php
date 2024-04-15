013291c930ec498ad750524558bd34222b8a25d2
Kamilla <KamillaKa@users.noreply.github.com>
2024-04-15 18:43:57 +0300

    main

--- week2/database/insertData.php
+++ week2/database/insertData.php
@@ -1,22 +1,28 @@
 <?php
+session_start();
+if (!isset($_SESSION['user'])) {
+    header('Location: index.php');
+    exit;
+}
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 global $DBH;
 require 'dbConnect.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['title']) && isset($_POST['description']) && $_FILES['file'] != null) {
+    if (isset($_POST['title']) && isset($_POST['description']) && $_FILES['file'] !== null) {
         $filename = $_FILES['file']['name'];
         $filetype = $_FILES['file']['type'];
         $filesize = $_FILES['file']['size'];
-        $filetmp = $_FILES['file']['tmp_name'];
+        $temp_file = $_FILES['file']['tmp_name'];
         $destination = __DIR__ . '/uploads/' . $filename;
-        if (!move_uploaded_file($filetmp, $destination)) {
-            header('Location: index.php?success=File upload failed');
+        if (!move_uploaded_file($temp_file, $destination)) {
+            // header('Location: home.php?success=File upload failed');
+            exit;
         }
 
         $data = [
-            'user_id' => 1,
+            'user_id' => $_SESSION['user']['user_id'],
             'filename' => $filename,
             'media_type' => $filetype,
             'title' => $_POST['title'],
@@ -30,8 +36,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         try {
             $STH = $DBH->prepare($sql);
             $STH->execute($data);
-            header('Location: index.php?success=Item added');
-        } catch (PDOException $e){
+            header('Location: home.php?success=Item added');
+        } catch (PDOException $e) {
             echo "Could not insert data into the database.";
             file_put_contents('PDOErrors.txt', 'insertData.php - ' . $e->getMessage(), FILE_APPEND);
         }
